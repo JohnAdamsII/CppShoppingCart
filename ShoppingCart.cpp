061921089e8a6d333a91c41c9276a3868d9153c0
John Adams <Johnadams@mail.usf.edu>
2023-04-13 19:04:45 +0000

    formatting

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -4,18 +4,20 @@
 #include <vector>
 #include <string>
 
-enum class ItemType {
+enum class ItemType
+{
     MusicCD,
     Book,
     Magazine
 };
 
-class Item {
+class Item
+{
 public:
-    Item(const std::string& name, double price, ItemType type) : name_(name), price_(price), type_(type) {}
+    Item(const std::string &name, double price, ItemType type) : name_(name), price_(price), type_(type) {}
     virtual ~Item() = default;
     virtual double getPrice() const { return price_; }
-    const std::string& getName() const { return name_; }
+    const std::string &getName() const { return name_; }
     ItemType getType() const { return type_; }
 
 protected:
@@ -24,57 +26,68 @@ protected:
     ItemType type_;
 };
 
-
-class MusicCD : public Item {
+class MusicCD : public Item
+{
 public:
-    MusicCD(const std::string& name, double price) : Item(name, price, ItemType::MusicCD) {}
+    MusicCD(const std::string &name, double price) : Item(name, price, ItemType::MusicCD) {}
     double getPrice() const override { return price_; }
 };
 
-class Book : public Item {
+class Book : public Item
+{
 public:
-    Book(const std::string& name, double price) : Item(name, price, ItemType::Book) {}
+    Book(const std::string &name, double price) : Item(name, price, ItemType::Book) {}
     double getPrice() const override { return price_; }
 };
 
-class Magazine : public Item {
+class Magazine : public Item
+{
 public:
-    Magazine(const std::string& name, double price) : Item(name, price, ItemType::Magazine) {}
+    Magazine(const std::string &name, double price) : Item(name, price, ItemType::Magazine) {}
     double getPrice() const override { return price_; }
 };
 
-
-class ShoppingCart {
+class ShoppingCart
+{
 public:
-    void addItem(std::unique_ptr<Item> item) {
+    void addItem(std::unique_ptr<Item> item)
+    {
         m_items.emplace_back(std::move(item));
     }
 
-    void addItems(std::vector<std::unique_ptr<Item>>& items) {
-        for (auto& item : items) {
+    void addItems(std::vector<std::unique_ptr<Item>> &items)
+    {
+        for (auto &item : items)
+        {
             m_items.emplace_back(std::move(item));
         }
     }
 
-    void writeContentsToFile(const std::string& filename) {
+    void writeContentsToFile(const std::string &filename)
+    {
         std::ofstream file(filename);
-        if (file.is_open()) {
-            for (const auto& item : m_items) {
+        if (file.is_open())
+        {
+            for (const auto &item : m_items)
+            {
                 file << item->getName() << " - $" << item->getPrice() << std::endl;
             }
             file.close();
         }
     }
 
-    void summarizeContents() {
+    void summarizeContents()
+    {
         double totalCDs = 0;
         double totalBooks = 0;
         double totalMagazines = 0;
         double grandTotal = 0;
         int cdCount = 0, bookCount = 0, magazineCount = 0;
 
-        for (const auto& item : m_items) {
-            switch (item->getType()) {
+        for (const auto &item : m_items)
+        {
+            switch (item->getType())
+            {
             case ItemType::MusicCD:
                 totalCDs += item->getPrice();
                 cdCount++;
@@ -102,7 +115,8 @@ private:
     std::vector<std::unique_ptr<Item>> m_items;
 };
 
-int main() {
+int main()
+{
     ShoppingCart cart;
     cart.addItem(std::make_unique<MusicCD>("A really cool CD", 10.99));
     cart.addItem(std::make_unique<Book>("A good book", 15.99));
